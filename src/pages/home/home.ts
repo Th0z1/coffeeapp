18cbe9513dddebf0bdb1735a151c90bf6b91e713
Mthokozisi <mthokozisimahlangu0@gmail.com>
2018-06-28 14:35:14 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,54 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  results= 0;
+  name:string;
+  whip:boolean;
+ choc:boolean;
+  wc:string;
+  ch:string;
+  price:number;
+  value = 0;
   constructor(public navCtrl: NavController) {
+  this.price= 10;
+  }
+  onReset(){
+    this.results = 0;
+    this.ch = "";
+    this.wc='';
+    this.whip=false;
+    this.choc=false;
+    this.name='';
 
   }
+  onPlus(){
+   this.results +=1;
+   console.log(this.results); 
+  }
+  onMinus(){
+  this.results -=1;
+  console.log(this.results);
+  }
+  onOrder(){
+    alert('Please Insert your Name First')
+      this.value=1;
+      this.price=this.price*this.results;
+    }
 
-}
+    onChoc(){
+      if(this.choc){
+        this.price+=5;
+        this.ch = "+Chocolate";
+      }else{
+        this.ch = "";
+    }}
+     onWhip(){
+        if(this.whip){
+          this.price+=5;
+        this.wc='+WhippedCream';
+      }else{this.wc='';
+        
+      }
+     }
+    }
+  
